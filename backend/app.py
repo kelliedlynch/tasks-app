703f630e540c0e116b9f85905bef962308931c17
Kellie Lynch <kellielynch@MacBook-Pro-2015.local>
2022-07-12 14:22:44 -0500

    Add and delete API in backend, starting to work on frontend

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 import sys
 # sys.path.insert(0,")/home/kellie/.local/bin")
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, Response
+import json
 import sqlite3 as sql
 
 app = Flask(__name__)
@@ -24,6 +25,47 @@ app = Flask(__name__)
 #       msg = "records added"
 #    return msg
 
+@app.route("/del-task", methods=["POST", "OPTIONS"])
+def delete_task():
+
+   operation = "preflight"
+   if request.method == "POST":
+      # jsondict = json.load(request.json)
+      print(type(request.json["taskid"]))
+      with sql.connect("database.db") as con:
+         cur = con.cursor()
+         cur.execute("DELETE FROM tasks WHERE taskid=?", [request.json["taskid"]] )
+         con.commit()
+         operation = "deleted task"
+         # con.close()
+
+   response = Response(operation)
+   response.access_control_allow_origin = "*"
+   response.access_control_allow_methods = ["POST", "OPTIONS"]
+   response.access_control_allow_headers = ["Content-Type"]
+   return response
+
+@app.route("/add-task", methods=["POST", "OPTIONS"])
+def add_task():
+
+   operation = "preflight"
+   if request.method == "POST":
+      # jsondict = json.load(request.json)
+      print(request.json["taskName"])
+      with sql.connect("database.db") as con:
+         cur = con.cursor()
+         cur.execute("INSERT INTO tasks (name, completed) VALUES (?, False)", [request.json["taskName"]] )
+         con.commit()
+         operation = "inserted task"
+         # con.close()
+
+   response = Response(operation)
+   response.access_control_allow_origin = "*"
+   response.access_control_allow_methods = ["POST", "OPTIONS"]
+   response.access_control_allow_headers = ["Content-Type"]
+   return response
+
+
 @app.get("/tasks")
 def show_tasks():
 
@@ -34,14 +76,14 @@ def show_tasks():
    rows = cur.fetchall();
 
    query_array = []
-   for i, row in enumerate(rows):
+   for row in rows:
       query_array.append ({
-         "id": i,
+         "taskid": row["taskid"],
          "name": row["name"],
          "completed": row["completed"]
       })
    query_results = jsonify(query_array)
-   query_results.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
+   query_results.headers.add('Access-Control-Allow-Origin', '*')
    return query_results
 
 # application = app
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,64 +1,143 @@
 //import logo from './logo.svg';
 //import './App.css';
-import React from 'react';
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 // const http = require("http");
 
 const BACKEND_URL = "http://localhost:5000/";
 const GET_API = "tasks";
+const DEL_API = "del-task";
 
-const demo_tasks = [
-  { id: 0, name: "task number one", completed: 0 },
-  { id: 1, name: "task number two", completed: 0 },
-  { id: 2, name: "task number three", completed: 0 },
-  { id: 3, name: "task number four", completed: 0 },
-];
+// const demo_tasks = [
+//   { id: 0, name: "task number one", completed: 0 },
+//   { id: 1, name: "task number two", completed: 0 },
+//   { id: 2, name: "task number three", completed: 0 },
+//   { id: 3, name: "task number four", completed: 0 },
+// ];
 
 // const live_tasks = () => {
   // const data = fetch(BACKEND_URL + GET_API)
-let live_tasks = []
+// let live_tasks = []
 
-fetch(BACKEND_URL + GET_API)
-    .then(response => response.json())
-    .then(data => {
-      live_tasks = data;
-      console.log(data);
-    });
+// fetch(BACKEND_URL + GET_API)
+//     .then(response => response.json())
+//     .then(data => {
+//       live_tasks = data;
+//       // console.log("data", data);
+//     });
 // console.log(live_tasks);
     // console.log(data);
     // return data.result;
 
 
+
 function App() {
-  console.log(live_tasks);
+  const [loading, setLoading] = useState(false);
+  const [listItems, setListItems] = useState([]);
+
+
+  useEffect(() => {
+    const initListItems = async () => {
+      setLoading(true);
+      const response = fetch(BACKEND_URL + GET_API)
+        .then(response => response.json())
+        .then(response => {
+          // console.log(response)
+            setListItems (response);
+            setLoading(false);
+        })
+      // const resJson = await response.json();
+      // console.log("resJson", resJson);
+
+
+    }
+    initListItems();
+  }, []);
+
+
+
+
+      // setLoading(true);
+      // const finalResponse = await fetch(BACKEND_URL + GET_API)
+      //   .then(response => response.json())
+      //   .then(response => {
+      //     setTasks(response.result);
+      //     return response.result;
+      //     console.log("response.result", response.result);
+      //   });
+      // const response = await fetch(BACKEND_URL + GET_API);
+      // const resJson = response.json();
+      // const result = resJson.result;
+      // console.log("response is", response.json());
+      // setTasks(response);
+      // setLoading(false);
+    // };
+    // initTasks();
+  //   return;
+  // }, []);
+
+
+
   return (
-    <Checklist listItems={live_tasks} />
+    <div>
+    {loading ? (
+      <h4>Loading...</h4>) :
+      <Checklist listItems={listItems} />
+  }
+  </div>
   );
 }
 
-// class Checklist extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.tasks = props.tasks;
-//   }
+
+
+
+
 function Checklist(props) {
   const [listItems, setListItems] = useState(props.listItems);
+
+  function addListItem(newItem) {
+    setListItems([...listItems, newItem]);
+  }
 
   useEffect(() => {
     setListItems(props.listItems);
   }, [props.listItems]);
+  console.log(props);
+
 
   return (
     <ul className = "taskList">
-      {listItems.map((listItem) =>
-        <ListItem key={listItem.id} item={listItem} />
+      {props.listItems.map((listItem) =>
+        <ListItem key={listItem.taskid} item={listItem} />
       )}
+      <li><AddListItemForm addListItem={addListItem} /></li>
     </ul>
   );
 }
 
+function AddListItemForm( { addListItem } ) {
+  const [newListItemName, setNewListItemName] = useState();
 
+  // useEffect(() => {
+  //   setNewListItemName();
+  // }, []);
+
+  function handleSubmit( event ) {
+    // TODO: what should the taskid of a new task be, if any?
+    addListItem( { taskid: -1, name: newListItemName, completed: 0 });
+    console.log("list item added", newListItemName);
+    // what does this do?
+    event.preventDefault();
+  }
+
+  return (
+    <form onSubmit={ e => {handleSubmit(e)}}>
+      <input type="text" name="newListItemName" value={newListItemName}
+        onChange={event => setNewListItemName(event.target.value)} />
+      <input type="submit" value="Add Item" />
+    </form>
+    );
+}
 
 function ListItem(props) {
   const [completed, setCompleted] = useState(props.item.completed);
@@ -66,11 +145,19 @@ function ListItem(props) {
   useEffect(() => {
     setCompleted(props.item.completed);
   }, [props.item.completed]);
+//
 
   function handleCheckbox () {
     console.log("checkbox clicked");
     if( completed === 0 ) {
-      setCompleted(1);
+      const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ taskid: props.item.taskid })
+      };
+      fetch(BACKEND_URL + DEL_API, requestOptions)
+      .then(setCompleted(1))
+
     } else if( completed === 1 ) {
       setCompleted(0);
     }
