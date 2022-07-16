ab8c70a9aa8b370015f5bb1014c71c6722187c6d
Kellie Lynch <kellielynch@MacBook-Pro-2015.local>
2022-07-15 22:27:45 -0500

    Trying to lift states up properly--not there yet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,86 +6,151 @@ import React, { useState, useEffect } from 'react';
 
 const BACKEND_URL = "http://localhost:5000/";
 const GET_API = "tasks";
-const DEL_API = "del-task";
+// const DEL_API = "del-task";
 const ADD_API = "add-task";
 const EDIT_API = "edit-item"
 
+function sortList( unsortedList ) {
+  let incompleteItems = [];
+  let completeItems = [];
+  unsortedList.forEach( item => {
+    if( item.completed === 0 ) {
+      incompleteItems.push(item);
+    } else {
+      completeItems.push(item);
+    };
+  });
+  return incompleteItems.concat(completeItems);
+}
+
 function App() {
+  // const [listItems, setListItems] = useState();
+
+  // // async function initListItems() {
+  //   // setLoading(true);
+  //   console.log("initListItems");
+  //   const response = await fetch(BACKEND_URL + GET_API);
+  //   const rawList = await response.json();
+  //   console.log("rawList", rawList);
+  //   const sortedList = sortList( rawList );
+  //   setListItems( sortedList );
+  //   // return sortedList;
+  //     // .then(response => response.json())
+  //     // .then(response => {
+  //     //     setListItems (response);
+  //     //     console.log("initListItems response was", response);
+  //     //     // setLoading(false);
+  //     // });
+  // // };
+  // // initListItems();
+  // console.log(listItems);
 
   return (
     <div>
-      <Checklist  />
+      <Checklist />
     </div>
   );
 };
 
 function Checklist(props) {
-  const [listItems, setListItems] = useState(props.listItems ? props.listItems : []);
+  const [listItems, setListItems] = useState([]);
 
-  async function initListItems() {
-    // setLoading(true);
-    console.log("initListItems");
-    fetch(BACKEND_URL + GET_API)
-      .then(response => response.json())
-      .then(response => {
-          setListItems (response);
-          console.log("initListItems response was", response);
-          // setLoading(false);
-      });
-  };
+  // async function initListItems() {
+  //   // setLoading(true);
+  //   console.log("initListItems");
+  //   const response = await fetch(BACKEND_URL + GET_API);
+  //   const rawList = await response.json();
+  //   const sortedList = sortList( rawList );
+  //   // setListItems( sortedList );
+  //   return sortedList;
 
-  useEffect(() => {
-    initListItems();
-  }, [props.listItems]);
-
-  function rerender() {
-    console.log("trying to rerender");
-    initListItems();
-  };
-
-  return (
-    <ul className = "checklist">
-      {listItems.map((listItem) =>
-        <ListItem key={listItem.item_id} item={listItem} rerender={rerender} />
-      )}
-      <li><AddListItemForm rerender={rerender} /></li>
-    </ul>
-  );
-}
-
-function ListItem(props) {
-  const [completed, setCompleted] = useState(props.item.completed);
+      // .then(response => response.json())
+      // .then(response => {
+      //     setListItems (response);
+      //     console.log("initListItems response was", response);
+      //     // setLoading(false);
+      // });
+  // };
+  // initListItems();
 
   useEffect(() => {
-    completeListItem();
-  }, [completed]);
-//
-
-  function handleCheckbox() {
-    if( completed == 0) {
-      setCompleted(1);
-    } else {
-      setCompleted(0);
+    async function initListItems() {
+      const response = await fetch(BACKEND_URL + GET_API);
+      const rawList = await response.json();
+      const sortedList = sortList( rawList );
+      setListItems(sortedList);
     }
-  }
+    initListItems();
+  }, []);
 
-  function completeListItem() {
+  async function toggleCompleted( item ) {
+    if( item.completed === 0 ) {
+      item.completed = 1;
+    } else {
+      item.completed = 0;
+    };
     const requestOptions = {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         do: "update_completed",
-        item_id: props.item.item_id,
-        completed: completed,
+        item_id: item.item_id,
+        completed: item.completed,
          })
       };
-      fetch(BACKEND_URL + EDIT_API, requestOptions);
-  } 
+      await fetch(BACKEND_URL + EDIT_API, requestOptions);
+      let newItems = [...listItems];
+      console.log("newItems before update", newItems);
+      newItems.forEach(thisItem => {
+        if( thisItem.item_id === item.item_id ) {
+          thisItem.completed = item.completed;
+        }
+      });
+      console.log("newItems after update", sortList(newItems));
+      setListItems(sortList(newItems));
+  }
+
+  return (
+    <ul className = "checklist">
+      {listItems.map((listItem) =>
+        <ListItem key={listItem.item_id} listItem={listItem} toggleCompleted={toggleCompleted} />
+      )}
+      <li><AddListItemForm setListItems={setListItems} /></li>
+    </ul>
+  );
+}
+
+function ListItem( props ) {
+  // const [listItem, setListItem] = useState(props.item);
+  // const [completed, setCompleted] = useState(props.item.completed);
+  // const item_id = props.item.item_id;
+
+  useEffect(() => {
+    // function completeListItem() {
+    //   const requestOptions = {
+    //     method: 'POST',
+    //     headers: { 'Content-Type': 'application/json' },
+    //     body: JSON.stringify({
+    //       do: "update_completed",
+    //       item_id: listItem.item_id,
+    //       completed: listItem.completed,
+    //        })
+    //     };
+    //     fetch(BACKEND_URL + EDIT_API, requestOptions);
+    // }
+    // completeListItem();
+  }, );
+//
+
+  function handleCheckbox() {
+    props.toggleCompleted( props.listItem );
+    // props.rerender();
+  }
 
   return (
     <li>
-      <input type="checkbox" checked={completed} onChange={handleCheckbox} />
-      {props.item.name}
+      <input type="checkbox" checked={props.listItem.completed} onChange={handleCheckbox} />
+      {props.listItem.name}
     </li>
   );
 }
@@ -109,11 +174,10 @@ function AddListItemForm(props) {
       body: JSON.stringify({ name: newListItemName })
     };
 
-    let response = await fetch(BACKEND_URL + ADD_API, requestOptions);
+    await fetch(BACKEND_URL + ADD_API, requestOptions);
     props.rerender();
     setNewListItemName("");
 
-    console.log("list item added:", newListItemName, "response:", response);
   }
 
   return (
